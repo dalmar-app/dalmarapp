b9ff94da6710a9c3d0e6743ebc3cfd171bbdb016
dalmar-app <ahmedabdirisakalijaamac@gmail.com>
2026-02-03 11:41:40 -0800

    Update DriverDashboard.jsx

--- src/DriverDashboard.jsx
+++ src/DriverDashboard.jsx
@@ -1,22 +1,23 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-// Soo kaxaynta boggaga
-import HomePage from './components/HomePage'; 
-import DriverDashboard from './DriverDashboard'; 
-
-function App() {
+const DriverDashboard = () => {
   return (
-    <Router>
-      <Routes>
-        {/* Bogga dadweynaha (Customer View) */}
-        <Route path="/" element={<HomePage />} />
-        
-        {/* Bogga darawalka (Driver Dashboard) */}
-        <Route path="/drivers" element={<DriverDashboard />} />
-      </Routes>
-    </Router>
+    <div style={{ 
+      padding: '50px', 
+      textAlign: 'center', 
+      fontFamily: 'sans-serif',
+      color: '#000000', // Qoraal madow
+      backgroundColor: '#ffffff', // Bog cad (si aan u aragno inuu shaqaynayo)
+      minHeight: '100vh'
+    }}>
+      <h1 style={{ color: 'red' }}>DALMAR DRIVER DASHBOARD</h1>
+      <p>Haddii aad qoraalkan aragto, boggu wuu shaqaynayaa!</p>
+      <div style={{ marginTop: '20px', padding: '20px', border: '2px solid black' }}>
+        <h3>Dalabaadka Hadda Jira</h3>
+        <p>Ma jiraan dalabaad cusub...</p>
+      </div>
+    </div>
   );
-}
+};
 
-export default App;
+export default DriverDashboard;
