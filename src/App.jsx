8be15d913a1f07c1ef66667997eb73f5c20a62aa
dalmar-app <ahmedabdirisakalijaamac@gmail.com>
2026-02-03 12:26:32 -0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-// Soo kaxaynta boggaga
-import HomePage from './components/HomePage'; 
-import DriverDashboard from './DriverDashboard'; 
+import HomePage from './components/HomePage';
+import DriverDashboard from './DriverDashboard';
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* Bogga dadweynaha (Customer View) */}
         <Route path="/" element={<HomePage />} />
-        
-        {/* Bogga darawalka (Driver Dashboard) */}
         <Route path="/drivers" element={<DriverDashboard />} />
       </Routes>
     </Router>
