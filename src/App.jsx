af3bb2328953bd85009c657f6916b1cfd164e665
dalmar-app <ahmedabdirisakalijaamac@gmail.com>
2026-02-03 20:35:55 -0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,36 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-// HALKAN AYAA CILADDU JIRTAY - WAA INAAN SI SAX AH U TILMAAMNAA GALKA
-import Home from './components/HomePage'; 
-
+// 1. Soo jiidashada Boggaga (Imports)
+// HUBI: HomePage wuxuu ku dhex jiraa galka 'components'
+import HomePage from './components/HomePage'; 
 import DriverDashboard from './DriverDashboard';
 import AdminPanel from './AdminPanel';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        {/* Bogga hore oo hadda laga soo aqrinayo galka components */}
-        <Route path="/" element={<Home />} />
-        
-        <Route path="/drivers" element={<DriverDashboard />} />
-        <Route path="/admin-secret-page" element={<AdminPanel />} />
-      </Routes>
+      <div className="App">
+        <Routes>
+          {/* BOGGA HORE (Home): 
+            Kani waa kan automatic-ga u garanaya magaalada (Garowe, Muqdisho, etc.)
+          */}
+          <Route path="/" element={<HomePage />} />
+
+          {/* BOGGA DARAWALKA (Driver): 
+            Halkan ayuu darawalku ka dooranayaa magaalada uu joogo
+          */}
+          <Route path="/drivers" element={<DriverDashboard />} />
+
+          {/* BOGGA MAAMULKA (Admin): 
+            Halkan ayaad kaga kormeeraysaa dhamaan magaalooyinka
+          */}
+          <Route path="/admin-secret-page" element={<AdminPanel />} />
+          
+          {/* Haddii qofku link khaldan galo, ku celi Home-ka */}
+          <Route path="*" element={<HomePage />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
